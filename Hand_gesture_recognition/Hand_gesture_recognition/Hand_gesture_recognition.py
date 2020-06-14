5c8c2206e7210c5c72f6662645ac90c3e9d7859c
Martika98 <maslok10@gmail.com>
2020-06-15 01:07:41 +0200

    Rozpoznawanie gestów
    Współczynniki zostały dopasowane do konkretnych gestów. Dodano
    obliczanie wartości kątów między palcami. Rozróżnienie kciuka w dół i w
    górę.

--- Hand_gesture_recognition/Hand_gesture_recognition/Hand_gesture_recognition.py
+++ Hand_gesture_recognition/Hand_gesture_recognition/Hand_gesture_recognition.py
@@ -34,7 +34,9 @@ w = lcnt/lhull #współczynnik obwodów
 acnt = cv2.contourArea(cnt) # powierzchnia konturu
 ahull = cv2.contourArea(hull2)
 aq = acnt/ahull # współczynnik powierzchni
-
+# fin to współrzędna y od punktu w miejscu zgjęcia kciuka (w wielokącie)
+fin1 = 0 #punkty sąsiadujace do fin
+fin2 = 0
 defects = cv2.convexityDefects(cnt, hull) # defekty hull
 n = defects.shape[0]
 for i in range (n):
@@ -51,61 +53,83 @@ for i in range (n):
     cosang = (b**2 + c**2 - a**2) / (2*b*c) # cosinus kąta między palcami rąk
     ang = math.acos(cosang) # kąt między palcami dłoni swej
 
-    if( lngratio > 4.1): # odległość jest wystarczająco duża
+    if( lngratio > 4.0): # odległość jest wystarczająco duża
         cv2.line(image2, start, end, [0,230, 230], 2)
         cv2.circle(image2, far, 5, [0, 0, 255], -1)
         l = l +1
-        if (ang < 1.4): # kąt ostry 
+        if (ang < 1.3): # kąt ostry 
             lo = lo + 1
-        if (ang > 1.4 and ang < 1.65): # kąt prosty
+        if (ang > 1.3 and ang < 1.75): # kąt prosty
             rangle = 1
+            fin1 = start[1]
+            fin2 = end[1]
 
 approx = cv2.approxPolyDP(cnt, 0.0185*cv2.arcLength(cnt, True), True) # przybliżenie konturu wielokątem
 cv2.drawContours(image2, [approx], -1, (255,0 ,0), 3)
+maxy = 0
+miny = height
+
+
+for app in approx:
+    _, y = app[0]
+    if maxy < y :
+        maxy = y
+    if miny > y :
+        miny = y
+
+
+
+
+
 lk = len(approx) # liczba krawędzi w wielokącie 
 
-if lo == 0:
-    if w > 1.09 and w < 1.27 and lk < 7 and aq < 0.8:
-        cv2.putText(image2,'Wskazywanie',(50,100), font, 2, (0,0,255), 3, cv2.LINE_AA)
+if lo == 0 and rangle == 0:
+    if w > 1.09 and w < 1.38 and lk < 9 and aq < 0.8:
+        cv2.putText(image2,'Wskazywanie',(50,100), font, 2, (128,0,255), 5, cv2.LINE_AA)
 if lo == 1 or lo == 0:
     if lk > 7 and lk < 10:
         if aq > 0.5 and aq < 0.62:
             if w > 1.27:
-                cv2.putText(image2,'Legiunia',(50,100), font, 2, (0,0,255), 3, cv2.LINE_AA)
+                cv2.putText(image2,'Legiunia',(180,100), font, 2, (128,0,255), 5, cv2.LINE_AA)
 if lo == 1:
     if l == 2 or l==1:
         if w > 1.21 and w < 1.35:
             if lk == 6 or lk == 7:
-                cv2.putText(image2,'Dwa',(50,1000), font, 2, (0,0,255), 3, cv2.LINE_AA)
+                cv2.putText(image2,'Dwa',(50,250), font, 2, (128,0,255), 5, cv2.LINE_AA)
 if lo == 2:
     if lk == 9 or lk == 8:
         if l==3 or l == 4 or l==2:
             if w < 1.4 and w > 1.3:
-                cv2.putText(image2,'Trzy',(50,100), font, 2, (0,0,255), 3, cv2.LINE_AA)
+                cv2.putText(image2,'Trzy',(180,250), font, 2, (128,0,255), 5, cv2.LINE_AA)
 if lo == 3:
     if lk == 9 or lk == 10 or lk == 11:
         if w > 1.4 and w < 1.6:
-            cv2.putText(image2,'Cztery',(50,100), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            cv2.putText(image2,'Cztery',(50,350), font, 2, (128,0,255), 5, cv2.LINE_AA)
+if w > 2.1:
+    cv2.putText(image2,'Machanie',(180,350), font, 2, (128,0,255), 5, cv2.LINE_AA)
 if lo ==4 and lk > 1.8:
     if aq > 0.59 and aq < 0.78:
         if lk > 8:
-            cv2.putText(image2,'Machanie',(50,100), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            cv2.putText(image2,'Machanie',(180,350), font, 2, (128,0,255), 5, cv2.LINE_AA)
     if aq > 0.78:
-            cv2.putText(image2,'Piatka',(50,100), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            cv2.putText(image2,'Piatka',(50,450), font, 2, (128,0,255), 5, cv2.LINE_AA)
 if aq < 0.58 and lk > 11 and w > 1.6:
-    cv2.putText(image2,'Machanie',(50,100), font, 2, (0,0,255), 3, cv2.LINE_AA)
+    cv2.putText(image2,'Machanie',(180,450), font, 2, (128,0,255), 5, cv2.LINE_AA)
 if lk ==7 and lo == 1:
     if w > 1.28 and w < 1.4:
-        cv2.putText(image2,'Przekazany znak pokoju',(50,100), font, 2, (0,0,255), 3, cv2.LINE_AA)
-if rangle == 1:
-    cv2.putText(image2,'OK',(50,100), font, 2, (0,0,255), 3, cv2.LINE_AA)
+        cv2.putText(image2,'Przekazany znak pokoju',(50,550), font, 5, (128,0,255), 3, cv2.LINE_AA)
+if rangle == 1 and aq > 0.65 and lo < 2:
+    if ((maxy < fin1 +100 and maxy > fin1 - 100) or (maxy < fin2 + 100 and maxy > fin2 - 100)): # jeśli któryś z puntów sąsiadujacych jest najwyższym punktem wielokąta, to bardzoprawdopodobne, ze jest to punkt kciuka w dół
+        cv2.putText(image2,'Nie ok',(180,550), font, 2, (128,0,255), 5, cv2.LINE_AA)
+    if ((miny < fin1 +100 and miny > fin1 - 100) or (miny < fin2 + 100 and miny > fin2 - 100)): # jeśli któryś z punktów sąsiadujących jest najniżej, to prawdopodobnie jest to kciuk w górę
+        cv2.putText(image2, 'OK', (50,650), font, 2, (128,0,255), 5, cv2.LINE_AA)
 if lo == 0 and l ==0:
-    if aq > 0.75:
-        cv2.putText(image2,'Piesc',(50,100), font, 2, (0,0,255), 3, cv2.LINE_AA)
+    if aq > 0.8 and w <1.35:
+        cv2.putText(image2,'Piesc',(180,650), font, 2, (128,0,255), 5, cv2.LINE_AA)
 if l == 1:
-    if lo == 1 or lo == 0:
+    if lo == 0:
         if lk == 6 or lk == 5:
-            cv2.putText(image2,'Znak szatana',(50,100), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            cv2.putText(image2,'Znak szatana',(50,750), font, 2, (128,0,255), 5, cv2.LINE_AA)
 
 
 print (lo)
@@ -115,6 +139,12 @@ print (l)
 print (w)
 print (aq)
 print (rangle)
+
+print (fin1)
+print (fin2)
+print (miny)
+print (maxy)
+
 
 
 cv2.imshow('image', image2)
